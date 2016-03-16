a13ad0473bb2321e6cc2b6d28212a9fc4ed7d70b
ruanqin <ruanqin@haizhi.com>
2016-03-16 23:49:13 +0800

    我在test分支上修改了MainActivity

--- app/src/main/java/com/rqrqrrq/test/MainActivity.java
+++ app/src/main/java/com/rqrqrrq/test/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("Main","LLLLLLLLLA");
+
+        Log.d("嘻嘻","what are you doing know");
     }
 }
